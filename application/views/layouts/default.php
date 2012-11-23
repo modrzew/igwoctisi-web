35b29f9701dd1e49a206cc46903b8ee2667493e2
Modrzew <dev@modriv.net>
2012-11-23 12:57:21 +0100

    Podstawowy layout + placeholdery dla styli i skryptow

---
+++ application/views/layouts/default.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+	<head>
+		<script type="text/javascript" src="<?= Url::base() ?>assets/js/jquery-1.8.3.min.js"></script>
+		<script type="text/javascript" src="<?= Url::base() ?>assets/js/less-1.3.1.min.js"></script>
+		<script type="text/javascript" src="<?= Url::base() ?>assets/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="<?= Url::base() ?>assets/js/main.js"></script>
+		<link rel="stylesheet/css" type="text/css" href="<?= Url::base() ?>assets/styles/bootstrap-responsive.min.css" />
+		<link rel="stylesheet/less" type="text/less" href="<?= Url::base() ?>assets/styles/style.less" />
+		<title>IGWOCTISI</title>
+	</head>
+	
+	<body>
+<?= $content ?>
+	</body>
+</html>
