f4c94dd0f5b93cd6867db8dba791593868ebdb5d
Modrzew <dev@modriv.net>
2012-11-24 19:48:18 +0100

    Logowanie, rejestracja

---
+++ application/views/partials/login.php
@@ -0,0 +1,23 @@
+<h1>Login</h1>
+
+<?php if(isset($loginFailed)) echo '<p class="text-error">Wrong username and/or password.</p>'; ?>
+
+<form class="form-horizontal" method="POST">
+	<div class="control-group">
+		<label class="control-label" for="username">Username</label>
+		<div class="controls">
+			<input type="text" id="username" name="username" value="<?php if(isset($data)) echo $data['username']; ?>" />
+		</div>
+	</div>
+	<div class="control-group">
+		<label class="control-label" for="password">Password</label>
+		<div class="controls">
+			<input type="password" id="password" name="password" value="<?php if(isset($data)) echo $data['password']; ?>" />
+		</div>
+	</div>
+	<div class="control-group">
+		<div class="controls">
+			<button type="submit" class="btn btn-success">Login</button></span>
+		</div>
+	</div>
+</form>
