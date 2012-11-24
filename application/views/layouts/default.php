f20db5b06c9f7432e2ec978d9fcad8611186ec89
Modrzew <dev@modriv.net>
2012-11-24 18:36:05 +0100

    Strona glowna

--- application/views/layouts/default.php
+++ application/views/layouts/default.php
@@ -1,16 +1,30 @@
-<!doctype html>
+<!DOCTYPE html>
 <html>
 	<head>
 		<script type="text/javascript" src="<?= Url::base() ?>assets/js/jquery-1.8.3.min.js"></script>
-		<script type="text/javascript" src="<?= Url::base() ?>assets/js/less-1.3.1.min.js"></script>
 		<script type="text/javascript" src="<?= Url::base() ?>assets/js/bootstrap.min.js"></script>
 		<script type="text/javascript" src="<?= Url::base() ?>assets/js/main.js"></script>
-		<link rel="stylesheet/css" type="text/css" href="<?= Url::base() ?>assets/styles/bootstrap-responsive.min.css" />
+		<link rel="stylesheet" type="text/css" href="<?= Url::base() ?>assets/styles/bootstrap.min.css" />
 		<link rel="stylesheet/less" type="text/less" href="<?= Url::base() ?>assets/styles/style.less" />
+		<script type="text/javascript" src="<?= Url::base() ?>assets/js/less-1.3.1.min.js"></script>
+		<script type="text/javascript" src="<?= Url::base() ?>assets/js/main.js"></script>
+		<meta charset="utf-8">
 		<title>IGWOCTISI</title>
 	</head>
-	
+
 	<body>
-<?= $content ?>
+		<div class="container">
+			<div class="masthead">
+				<ul class="nav nav-pills pull-right">
+<!-- 					<li class="active"><a href="#">Main</a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="#">Contact</a></li> -->
+				</ul>
+				<h3 class="muted">IGWOCTISI</h3>
+			</div>
+			
+			<?= $content ?>
+
+		</div>
 	</body>
 </html>
---
+++ application/views/partials/mainNotLogged.php
@@ -0,0 +1,6 @@
+<div class="jumbotron">
+	<h1>Conquer the galaxy!</h1>
+	<p class="lead">And show other players you wanna be the very best, like no one ever was;<br />To conquer them is your real test, to crush them is your cause.</p>
+	<a class="btn btn-primary btn-large" href="<?= Url::base() ?>signup">Sounds awesome, let me in!</a>
+	<a class="btn" href="<?= Url::base() ?>login">I'm already a conqueror</a>
+</div>
