11aa54d4d9718747c40ab7c64e6e0687d9b826d9
Modrzew <dev@modriv.net>
2012-11-28 14:28:18 +0100

    Wiecej info na stronach

--- application/classes/Model/User.php
+++ application/classes/Model/User.php
@@ -7,4 +7,27 @@ class Model_User extends Model_Auth_User {
 		'places'		=> array('model' => 'Place'),
 		'games'		=> array('model' => 'Game', 'through' => 'places'),
 	);
+	
+	public function getRankingPosition()
+	{
+		if(!$this->loaded())
+			throw new Kohana_Exception('User object not loaded');
+		
+		$ranking = ORM::factory('user')->order_by('points', 'DESC')->order_by('username', 'ASC')->find_all();
+		$place = 0;
+		$hiddenPlace = 0;
+		$lastPoints = PHP_INT_MAX;
+		foreach($ranking as $u)
+		{
+			$hiddenPlace++;
+			if($u->points < $lastPoints)
+			{
+				$place = $hiddenPlace;
+				$lastPoints = $u->points;
+			}
+			if($u->username === $this->username)
+				break;
+		}
+		return $place;
+	}
 }
--- application/views/partials/game.php
+++ application/views/partials/game.php
@@ -1,2 +1,14 @@
 <h1><?= $game->name ?></h1>
 
+<ul>
+	<li>Finished on <?= date('F j, Y', strtotime($game->date_played)) ?> at <?= date('H:i', strtotime($game->date_played)) ?></li>
+	<li>It took <?= $game->getTime() ?> to finish this game</li>
+</ul>
+
+<h3>Places</h3>
+
+<ol>
+<?php foreach($game->places->order_by('place', 'ASC')->find_all() as $p): ?>
+	<li><a href="<?= Url::base() ?>profile/<?= $p->user->username ?>"><?= $p->user->username ?></a> (<?= ($p->points < 0) ? '-' : (($p->points > 0) ? '+' : '') ?><?= $p->points ?>)</li>
+<?php endforeach; ?>
+</ol>
