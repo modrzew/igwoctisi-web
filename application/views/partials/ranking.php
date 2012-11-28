cf0d2d04e9e710623540317fda5f48f284d48b91
Modrzew <dev@modriv.net>
2012-11-28 14:32:22 +0100

    Url::base() -> URL::base()

--- application/views/partials/ranking.php
+++ application/views/partials/ranking.php
@@ -24,7 +24,7 @@
 ?>
 		<tr <?php if(($user = Auth::instance()->get_user(FALSE)) AND $user->username == $u->username) echo 'class="info"'; ?>>
 			<td><?= $place ?></td>
-			<td><a href="<?= Url::base()?>profile/<?= $u->username ?>"><?= $u->username ?></a></td>
+			<td><a href="<?= URL::base()?>profile/<?= $u->username ?>"><?= $u->username ?></a></td>
 			<td><?= $u->games->count_all() ?></td>
 			<td><?= $u->points ?></td>
 		</tr>
